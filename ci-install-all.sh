425dc17949634124e472b2c82b4cde168d45d682
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-07-15 16:37:34 -0400

    ci: harden toolchain npm detection

--- ci-install-all.sh
+++ ci-install-all.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-node scripts/check-toolchain.js
+npm run check:toolchain
 npm ci --legacy-peer-deps
 npm ci --legacy-peer-deps --prefix packages/yoroi-extension
 npm ci --prefix packages/e2e-tests
